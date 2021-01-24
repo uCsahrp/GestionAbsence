1d784c1d499dc34d1c02668399569b727640d7aa
AYB-cmd <aeljelladi@gmail.com>
2021-01-24 15:42:50 +0100

    creating workspace

---
+++ GestionAbsence/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using GestionAbsence.ViewModels;
+namespace GestionAbsence
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
