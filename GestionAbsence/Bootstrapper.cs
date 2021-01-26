9d7e93533b59370c0b0773a6c3da3389cb04d851
MOUIGUINA MOHAMED <mohamedmouiguina7@gmail.com>
2021-01-26 01:50:49 +0100

    LoginPage Design

--- GestionAbsence/Bootstrapper.cs
+++ GestionAbsence/Bootstrapper.cs
@@ -16,7 +16,7 @@ namespace GestionAbsence
         }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            DisplayRootViewFor<ShellViewModel>();
+            DisplayRootViewFor<LoginViewModel>();
         }
     }
 }
